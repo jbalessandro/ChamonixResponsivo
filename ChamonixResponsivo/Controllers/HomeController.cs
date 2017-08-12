5fff675200f251f91eb7822c33e2b19d3d8bab43
jbalessandro <jb.alessandro@gmail.com>
2017-08-12 19:36:28 -0300

    Chamonix Responsivo

--- ChamonixResponsivo/Controllers/HomeController.cs
+++ ChamonixResponsivo/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ChamonixResponsivo.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,18 +16,52 @@ namespace ChamonixResponsivo.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Restaurante()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Cardapio()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+
+        public ActionResult Localizacao()
+        {
+            var key = ConfigurationManager.AppSettings["googlemaps"].ToString();
+            var src = "https://maps.googleapis.com/maps/api/js?key=" + key + "&callback=initMap";
+            ViewBag.Gmap = src;
+            return View();
+        }
+
+        public ActionResult Contato()
+        {
+            return View(new ContatoViewModels());
+        }
+
+        [HttpPost]
+        public ActionResult Contato(ContatoViewModels model)
+        {
+            if (ModelState.IsValid)
+            {
+                System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
+
+                message.From = new System.Net.Mail.MailAddress("contato@chamonixfondue.com");
+                message.To.Add(new System.Net.Mail.MailAddress("contato@chamonixfondue.com"));
+
+                message.IsBodyHtml = true;
+                message.BodyEncoding = Encoding.UTF8;
+                message.Subject = "Chamonix - contato pelo site";
+                message.Body = model.Mensagem;
+
+                System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient();
+                client.Send(message);
+
+                ViewBag.Obrigado = "Obrigado pelo contato!";
+                return View(model);
+            }
+
+            return View(model);
         }
     }
 }
---
+++ ChamonixResponsivo/Models/ContatoViewModels.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChamonixResponsivo.Models
+{
+    public class ContatoViewModels
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage ="Informe seu nome")]
+        public string Nome { get; set; }
+
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Informe seu e-mail")]
+        [DataType(DataType.EmailAddress, ErrorMessage ="Informe um email válido")]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Digite sua mensagem")]
+        [DataType(DataType.MultilineText)]
+        public string Mensagem { get; set; }
+    }
+}
