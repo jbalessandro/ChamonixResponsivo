c1e3c55dc52814b6058252ddb6ec82afc524013b
jbalessandro <jb.alessandro@gmail.com>
2017-09-02 19:17:40 -0300

    Namespaces

--- ChamonixResponsivo/Controllers/HomeController.cs
+++ ChamonixResponsivo/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
 ﻿using ChamonixResponsivo.Models;
-using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Text;
-using System.Web;
 using System.Web.Mvc;
 
 namespace ChamonixResponsivo.Controllers
